263fca396d86aecc16b83049b9f56196165b6759
Jose Delgado <jdelgado1025@gmail.com>
2022-07-25 22:24:12 -0700

    Modified model to account for other product properties

--- Models/ProductQueryParameters.cs
+++ Models/ProductQueryParameters.cs
@@ -4,5 +4,8 @@
     {
         public decimal? MinPrice { get; set; }
         public decimal? MaxPrice { get; set; }
+        public string Sku { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+
     }
 }
