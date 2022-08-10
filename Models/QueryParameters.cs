d0679be958915c12b48c006f6add09dcce899846
Jose Delgado <jdelgado1025@gmail.com>
2022-08-09 21:39:43 -0700

    Added new sorting parameters

--- Models/QueryParameters.cs
+++ Models/QueryParameters.cs
@@ -5,10 +5,33 @@
         //Private fields
         const int _maxSize = 100;
         private int _size = 50;
+        private string _sortOrder = "asc";
         public int Page { get; set; } = 1;
-        public int Size { 
-            get { return _size; }
-            set { _size = Math.Min(_maxSize, value); }
+        public int Size
+        { 
+            get {
+                return _size;
+            }
+            set {
+                _size = Math.Min(_maxSize, value);
+            }
+        }
+
+        public string SortBy { get; set; } = "Id";
+
+        public string SortOrder
+        {
+            get
+            {
+                return _sortOrder;
+            }
+            set
+            {
+                if(value == "asc" || value == "desc")
+                {
+                    _sortOrder = value;
+                }
+            }
         }
     }
 }
