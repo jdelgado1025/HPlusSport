07984c511dc505a99cd6ff082951b9830b8f2525
Jose Delgado <jdelgado1025@gmail.com>
2022-08-15 20:33:58 -0700

    Add a new search query parameter to simplify name search

--- Models/ProductQueryParameters.cs
+++ Models/ProductQueryParameters.cs
@@ -7,5 +7,7 @@
         public string Sku { get; set; } = String.Empty;
         public string Name { get; set; } = String.Empty;
 
+        public string Search { get; set; } = String.Empty;
+
     }
 }
