b0d0b2ee132ada2d58ec4c38d1d67cef948e2b10
Jose Delgado <jdelgado1025@gmail.com>
2022-08-16 22:19:19 -0700

    Removed unnecessary imports/using

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 //using System.Linq;
 //using System.Linq.Dynamic.Core;
-using System.Reflection;
 
 namespace HPlusSport.API.Controllers
 {
